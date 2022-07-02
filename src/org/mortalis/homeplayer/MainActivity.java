8fe1626989b4ffe685a246255ec6bde33c7ff13d
mortalis13 <stangerroman@gmail.com>
2022-07-02 12:30:14 +0200

    ignore icon click for folders

--- src/org/mortalis/homeplayer/MainActivity.java
+++ src/org/mortalis/homeplayer/MainActivity.java
@@ -976,6 +976,7 @@ public class MainActivity extends AppCompatActivity {
         iconContainer = rootView.findViewById(R.id.iconContainer);
         
         iconContainer.setOnClickListener((view) -> {
+          if (!this.item.isFile) return;
           this.item.isFavorite = !this.item.isFavorite;
           itemIndicator.setVisibility(this.item.isFavorite ? View.VISIBLE: View.GONE);
           updateItemFavorite(this.item.path, this.item.isFavorite);
