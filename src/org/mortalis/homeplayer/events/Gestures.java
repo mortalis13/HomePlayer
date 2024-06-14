a0f383c82abf7cd56aaf2e4e2e37ccefa45f8bb2
mortalis13 <stangerroman@gmail.com>
2024-06-14 18:42:56 +0200

    added swipe limit on vertical axis for horizontal swipe

--- src/org/mortalis/homeplayer/events/Gestures.java
+++ src/org/mortalis/homeplayer/events/Gestures.java
@@ -2,10 +2,12 @@ package org.mortalis.homeplayer.events;
 
 import android.view.MotionEvent;
 
+import static org.mortalis.homeplayer.Fun.log;
 
 public class Gestures {
   
   private static final int SWIPE_THRESHOLD = 100;
+  private static final int SWIPE_LIMIT_WINDOW = 100;
   private static final int SWIPE_VELOCITY_THRESHOLD = 100;
   
   public static boolean isSwipedLeft(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
@@ -29,6 +31,7 @@ public class Gestures {
       Math.abs(diffX) > Math.abs(diffY) &&
       Math.abs(diffX) > SWIPE_THRESHOLD &&
       Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD &&
+      Math.abs(diffY) < SWIPE_LIMIT_WINDOW &&
       diffX > 0
     );
     return result;
